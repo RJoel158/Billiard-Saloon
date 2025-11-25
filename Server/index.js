45c12908c02e291f74f0f65b80979bee02298339
RJoel158 <svr0035567@est.univalle.edu>
2025-11-25 11:44:47 -0400

    Fix: Change front whit jsx to Tsx

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 300;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
